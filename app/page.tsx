fcab17328c0df4d7a83653e904f30ff352389188
Zelda <ze@zelda.sh>
2025-02-12 09:23:56 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,38 @@
+import Lair from "../public/lair.svg"
 import Image from "next/image";
+import { Germania_One } from "next/font/google";
+
+const germania = Germania_One({
+  weight: "400",
+  subsets: ["latin"],
+})
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex flex-col sm:flex-row w-full mt-8 gap-14 items-start">
+      <Image src={Lair} alt="Lair" className="w-[147px]" />
+      <main className="flex flex-col gap-4">
+        <div>
+          <h1 className="font-sans text-5xl leading-tight">ZELDA&apos;S LAIR</h1>
+          <div className={`${germania.className} text-2xl flex flex-col`}>
+            <a className="cursor-pointer ">About</a>
+            <a className="cursor-pointer opacity-75">Projects</a>
+            <a className="cursor-not-allowed opacity-75">Blog</a>
+          </div>
+        </div>
+        <div className="pt-2.5 flex flex-col gap-3">
+          <h1 className="font-sans text-4xl">ABOUT</h1>
+          <p className="text-lg">
+            Greetings, weary traveler! I’m <em>Zelda</em>, a 25-years-old conjurer of code, crafter of curiosities, digital enchantress, and ever-curious <em>witch</em> with an insatiable thirst for unraveling the mysteries that dance in the shadows of the unknown.
+          </p>
+        </div>
+        <div className="pt-2.5 flex flex-col gap-3">
+          <h1 className="font-sans text-4xl">CONTACT</h1>
+          <p className="text-lg">
+            Should you seek my talents, they are yours—for a price, of course. Summon me with a message, be it by enchanted scroll (email me at <em>hello@zelda.sh</em>) or through the mystical realms of Telegram or Discord, where I answer to <em>@zezelda</em>.
+          </p>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        serif: ["var(--font-basteleur-moonlight)"],
+        sans: ["var(--font-basteleur-bold)"],
+      },
     },
   },
   plugins: [],
